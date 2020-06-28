0d53c08ec9bac607595ba99ac01f094fb08c1ac7
Christian Consuelo <christian.consuelo2@gmail.com>
2020-06-27 19:01:31 -0500

    Add responsivness

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -10,9 +10,11 @@ import './index.css';
 const PageTitle = () => {
   return (
     <>
-      <div className="page-title">
-        <img alt="bio-pic" src={logo} />
-      </div>
+      <a href="/">
+        <div className="page-title">
+          <img className="bio-pic" alt="bio-pic" src={logo} />
+        </div>
+      </a>
       <div className="page-summary">
         <p>"La humanidad necesita gente sabia, no rica. La diferencia radica en que cuando se parte, un billete se parte, pero una idea se multiplica"</p>
         <p>-Danger AK</p>
