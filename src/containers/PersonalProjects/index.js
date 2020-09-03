d30db9e7d826ec2a5e70c069f9c496fa8caee191
Christian Consuelo <christian.consuelo2@gmail.com>
2020-09-03 09:55:26 -0500

    Add AlgoVis to projects list

--- src/containers/PersonalProjects/index.js
+++ src/containers/PersonalProjects/index.js
@@ -11,9 +11,17 @@ const PersonalProjects = () => {
   return (
     <>
       <Project
+        title={"AlgoVis"}
+        technologies={[
+          'JavaScript', 'D3.js', 'React.js', 'HTML', 'CSS'
+        ]}
+        description={"Algorithm visualization platform with D3. If you are a student or preparing a job interview this interactive platform will be helpful."}
+        url={'http://algovis.whileinfinito.com/'}
+      />
+      <Project
         title={"Conway's Game of Life"}
         technologies={[
-          'Javascript', 'Svelte', 'HTML', 'CSS'
+          'JavaScript', 'Svelte', 'HTML', 'CSS'
         ]}
         description={"A classic interactive implementation of the Conway's Game of Life. You can create your own automatas by using your mouse or drag and drop pre-defined automatas to check the different patterns you can play around."}
         url={'http://game-of-life.whileinfinito.com/'}
@@ -21,7 +29,7 @@ const PersonalProjects = () => {
       <Project
         title={"Chess"}
         technologies={[
-          'Javascript', 'Svelte', 'HTML', 'CSS'
+          'JavaScript', 'Svelte', 'HTML', 'CSS'
         ]}
         description={"A chess game you can access and play in a single computer. It already have all chess rules implemented and you can choose between different time limits."}
         url={'http://chess.whileinfinito.com/'}
@@ -29,7 +37,7 @@ const PersonalProjects = () => {
       <Project
         title={"Fast Memory Game"}
         technologies={[
-          'Javascript', 'Svelte', 'HTML', 'CSS', 'CSS animations'
+          'JavaScript', 'Svelte', 'HTML', 'CSS', 'CSS animations'
         ]}
         description={"Challenge your memory and play this Fast-Memory game implemente from scratch with CSS animation. You can play it from your mobile device too."}
         url={'http://fast-memory.whileinfinito.com/'}
@@ -37,7 +45,7 @@ const PersonalProjects = () => {
       <Project
         title={"Cookie Crasher"}
         technologies={[
-          'Javascript', 'Svelte', 'HTML', 'CSS', 'CSS animations'
+          'JavaScript', 'Svelte', 'HTML', 'CSS', 'CSS animations'
         ]}
         description={"In this game you will have to chase and click over bouncing cookies to get points. Invest those points to get more cookies and get the highest score you can!"}
         url={'https://cookie-crasher.whileinfinito.com/'}
